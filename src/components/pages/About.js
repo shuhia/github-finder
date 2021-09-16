481d4857ee8f71f8cf8a73b3cc8b6a313af27c25
Alex On <shuhia.on@gmail.com>
2021-09-16 20:12:12 +0200

    update version nr

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <>
       <h1> About This App</h1>
       <p> App to search Github users </p>
-      <p> Version: 1.0.0</p>
+      <p> Version: 1.0.1</p>
     </>
   );
 };
